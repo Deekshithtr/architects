1b08af83b7a057b6702eec113498060a72aac174
MohammadAshfan12 <mohammadashfan48@gmail.com>
2020-08-11 23:38:42 +0530

    Updates

---
+++ precache-manifest.a381ddaef3c8b13f2c48921d768e8235.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd118e99c598a2c6bfff663eb959c00",
+    "url": "/architects/index.html"
+  },
+  {
+    "revision": "c10ed735b4b1e8c37542",
+    "url": "/architects/static/css/main.b988c80c.chunk.css"
+  },
+  {
+    "revision": "12a751bd2a57a77d9b6e",
+    "url": "/architects/static/js/2.792bbad9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/architects/static/js/2.792bbad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c10ed735b4b1e8c37542",
+    "url": "/architects/static/js/main.8129e294.chunk.js"
+  },
+  {
+    "revision": "9cd33f5e5854080f4c11",
+    "url": "/architects/static/js/runtime-main.0a07789d.js"
+  },
+  {
+    "revision": "3da36579f9243fb3a951376b9e050622",
+    "url": "/architects/static/media/home.3da36579.jpg"
+  }
+]);
